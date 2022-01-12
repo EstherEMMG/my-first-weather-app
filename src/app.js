c491c61f4af518779499cf28dc51766fe0e36a48
EstherEMMG <96057903+EstherEMMG@users.noreply.github.com>
2022-01-12 12:26:44 +0100

    Added real forecast of 6 days

--- src/app.js
+++ src/app.js
@@ -21,6 +21,54 @@ function formatDate(timestamp) {
   }
   return `${day} ${date}  ${hour}:${minute}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+            <div class="card">
+              <div class="card-body">
+                <div class="weather-forecast-day">${formatDay(
+                  forecastDay.dt
+                )}</div>
+                <img src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png" alt="" width="42"/>
+                <div class="weather-forecast-temp"><span class="max-temp">${Math.round(
+                  forecastDay.temp.max
+                )}º</span><span class="min-temp">  ${Math.round(
+          forecastDay.temp.min
+        )}º</span></div>
+              </div>
+            </div>
+          </div>
+          `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "ed8f1b521e1ef205ffa5fdddc628ab7e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayWeatherCondition(response) {
   document.querySelector("h1").innerHTML = response.data.name;
@@ -43,6 +91,8 @@ function displayWeatherCondition(response) {
     .querySelector("#icon")
     .setAttribute("alt", response.data.weather[0].description);
   celsiusTemperature = Math.round(response.data.main.temp);
+
+  getForecast(response.data.coord);
 }
 
 function search(event) {
